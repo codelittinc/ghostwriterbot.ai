12a3aa1ea5c4609b72b61a4cd303903856cf5934
Omar Bahmad <omarbahmad@gmail.com>
2023-02-20 23:16:52 -0300

    feat: refactor Home Support and Privacy

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,24 @@
-var body = document.querySelector('body')
-var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
-var menuContainer = document.querySelector('#main-menu-mobile');
+var body = document.querySelector("body");
+var menuTrigger = document.querySelector("#toggle-main-menu-mobile");
+var menuContainer = document.querySelector("#main-menu-mobile");
 
-menuTrigger.onclick = function() {
-    menuContainer.classList.toggle('open');
-    menuTrigger.classList.toggle('is-active')
-    body.classList.toggle('lock-scroll')
-}
+menuTrigger.onclick = function () {
+  menuContainer.classList.toggle("open");
+  menuTrigger.classList.toggle("is-active");
+  body.classList.toggle("lock-scroll");
+};
 
 const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
+
+function changeVisibility(eachItem) {
+  const eachItemContainer = eachItem.querySelector(".faq-item-description");
+  const eachArrowImage = eachItem.querySelector(".arrow");
+  if (eachItemContainer.classList.value.includes("hide")) {
+    eachItemContainer.classList.remove("hide");
+    eachArrowImage.classList.add("rotate");
+  } else {
+    eachItemContainer.classList.add("hide");
+    eachArrowImage.classList.remove("rotate");
+  }
+}
